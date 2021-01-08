71e167811b7692626dd0cbff7b5267927ff2e906
jrkager <dschotze@gmail.com>
2021-01-08 23:43:12 +0100

    added git pull before commiting

--- loadvacc.sh
+++ loadvacc.sh
@@ -4,7 +4,8 @@ python vaccines-data-scraper-history.py
 if ! git diff --quiet vacc-history/P.A.\ Bolzano.csv
 then
         today=$(date +"%Y-%m_%d")
-        git add vacc-history/
+        git pull
+	git add vacc-history/
         git commit -m "update ${today}"
         git push
 fi
