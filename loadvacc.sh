feec4b150fc7c07618ca0bb01f64e760a14479c7
jrkager <dschotze@gmail.com>
2021-01-09 15:06:24 +0100

    typo

--- loadvacc.sh
+++ loadvacc.sh
@@ -7,7 +7,7 @@ python vaccines-data-scraper-history.py
 
 if ! git diff --quiet vacc-history/P.A.\ Bolzano.csv
 then
-        today=$(date +"%Y-%m_%d")
+        today=$(date +"%Y-%m-%d")
 	git add vacc-history/
         git commit -m "update ${today}"
         git push
