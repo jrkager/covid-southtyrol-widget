fc6a23abbb794a9070d90908446da7a8e5a3b1fa
jrkager <dschotze@gmail.com>
2021-01-10 16:08:48 +0100

    merged from data

--- update-history.py
+++ update-history.py
@@ -8,11 +8,14 @@ import importlib
 
 scraper = importlib.import_module("vaccines-data-scraper")
 
+def subst_last_row(loaded, vacc_count):
+    loaded["vcc"][-1] = vacc_count
+    calc(loaded)
+
 def add_row(loaded, vacc_count):
     for v in loaded.values():
         v.append('')
-    loaded["vcc"][-1] = vacc_count
-    calc(loaded)
+    subst_last_row(loaded, vacc_count)
 
 def calc(data):
     intervall=21
@@ -49,11 +52,47 @@ savefile_st_calc = "vacc-history/P.A. Bolzano.csv"
 date_vaccination_start = "2020-12-27"
 header = ["d","vcc","sum_1d","sum_monotone_1d","sum_2d","sum_monotone_2d"]
 
+# -- get new data --
+regjs = scraper.get_region_json()
+today = datetime.today().strftime('%Y-%m-%d')
+regjs["date"] = today
+
+# -- check specigic region --
 if not os.path.exists(savefile_st_calc):
     with open(savefile_st_calc,"w") as f:
         f.write(",".join(header) + "\n")
         f.write(",".join(["0"]*len(header)) + "\n")
 loaded = load_csv(savefile_st_calc)
+
+region_name = os.path.splitext(os.path.basename(savefile_st_calc))[0]
+today_count = regjs[region_name][0]
+print("Today counter:",today_count)
+
+# -- update calculations for specific region --
+# if not enough rows, fill with interpolation from the last inserted day up to today
+# (1 entry per day since vaccination start)
+diff = (datetime.fromisoformat(today)-datetime.fromisoformat(date_vaccination_start)).days
+days_of_vacc = diff + 1
+missing_days = days_of_vacc - (len(loaded["vcc"]) - 1)
+if missing_days > 0:
+    print("{}: add values for {} day(s)".format(region_name, missing_days))
+    interpolation = map(int,
+                     np.linspace(loaded["vcc"][-1], today_count, missing_days+1)[1:])
+    for count in interpolation:
+        # add row to csv data
+        add_row(loaded, count)
+elif loaded["vcc"][-1] != today_count:
+    # if we started the script for a second time this day, substitute last line with new data
+    print("{}: subsitute today with updated calculations".format(region_name))
+    subst_last_row(loaded, today_count)
+with open(savefile_st_calc, "w") as f:
+    cwr = csv.writer(f)
+    cwr.writerow(header)
+    for row in zip(*[loaded[k] for k in header]):
+        cwr.writerow(row)
+
+
+# -- update all-regions-stats file --
 latest_date = None
 try:
     with open(savefile_all, "r") as f:
@@ -61,32 +100,14 @@ try:
         latest_date = cont[-1]["date"]
 except:
     cont = []
-region_name = os.path.splitext(os.path.basename(savefile_st_calc))[0]
-today=datetime.today().strftime('%Y-%m-%d')
-regjs = scraper.get_region_json()
-regjs["date"]=today
-if not latest_date or today > latest_date:
+
+# only if something changed
+regjs = {k : list(v) if isinstance(v, tuple) else v for k, v in regjs.items()}
+
+if cont[-1] != regjs: # compare dicts in keys and vals
+    if today == latest_date:
+        print("subst last day in regions")
+        del cont[-1]
     cont.append(regjs)
-
-    today_count = regjs[region_name][0]
-    print("Today counter:",today_count)
-
-    # if not enough rows, fill with interpolation from the last inserted day up to today
-    # (1 entry per day since vaccination start)
-    diff = (datetime.fromisoformat(today)-datetime.fromisoformat(date_vaccination_start)).days
-    days_of_vacc = diff + 1
-    missing_days = days_of_vacc - (len(loaded["vcc"]) - 1)
-    if missing_days > 0:
-        interpolation = map(int,
-                         np.linspace(loaded["vcc"][-1], today_count, missing_days+1)[1:])
-        for count in interpolation:
-            # add row to csv data
-            add_row(loaded, count)
-
     with open(savefile_all, "w") as f:
         json.dump(cont, f)
-    with open(savefile_st_calc, "w") as f:
-        cwr = csv.writer(f)
-        cwr.writerow(header)
-        for row in zip(*[loaded[k] for k in header]):
-            cwr.writerow(row)
