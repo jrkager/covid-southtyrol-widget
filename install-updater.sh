85e3a561883961eef210825fb162f25e78c1289c
jrkager <dschotze@gmail.com>
2021-01-10 13:14:07 +0100

    automatically replace path and use sh to call

--- install-updater.sh
+++ install-updater.sh
@@ -5,6 +5,6 @@ git checkout data
 git pull
 
 crontab -l > mycron
-echo "23 00 * * * path/to/covid-southtyrol-widget/loadvacc.sh" >> mycron
+echo "23 00 * * * sh $(pwd)/loadvacc.sh" >> mycron
 crontab mycron
 rm mycron
